953e341f9e3acc79830cb3003bde3e6bc5180080
Vitaly Lipko <lipko-vitalij@yandex.ru>
2021-07-04 13:15:44 +0700

    chore: rename project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ta-root',
+  selector: 'tb-root',
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
@@ -28,5 +28,5 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'task-board';
 }
